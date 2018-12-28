08d715994a93dabb2fecbdbcb5064781e9cf3abe
root <christian.hernandez@yahoo.com>
2018-12-27 19:06:12 -0800

    Don't push this commit

---
+++ documents/golang/ninja9-2.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+type person struct {
+	Fname string
+	Lname string
+}
+
+func (p *person) speak() {
+	fmt.Println("Hello")
+}
+
+type human interface {
+	speak()
+}
+
+
+func saySomething(h human) {
+	h.speak()
+}
+
+func main() {
+	p1 := person {
+		Fname: "Christian",
+		Lname: "Hernandez",
+	}
+	//This won't work
+	/// saySomething(p1)
+
+	// This will work
+	saySomething(&p1)
+}
+
+
+
+//
+//-30-
---
+++ documents/golang/say-it.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func sayit(w string) {
+	fmt.Println(w)
+}
+
+func main() {
+	sayit("hello")
+}
