b54f39955ce22e0a9be4949a9edf4f22a1a7cb1e
Christian Hernandez <christian@chernand.io>
2024-07-14 18:27:08 -0700

    suttf
    Signed-off-by: Christian Hernandez <christian@chernand.io>

--- documents/golang/writer-interface.go
+++ documents/golang/writer-interface.go
@@ -1,1 +1,13 @@
-fun-with-printing.go
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	fmt.Println("Hello World")
+	fmt.Fprintln(os.Stdout, "Hello worlds")
+	io.WriteString(os.Stdout, "hello io\n")
+}
